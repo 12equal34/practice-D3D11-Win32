32b150ca6e4844dec110f60e6f7f99721bd30fb6
12equal34 <12equal34@gmail.com>
2023-05-21 22:53:58 +0900

    add Timer

--- Practice/Timer.cpp
+++ Practice/Timer.cpp
@@ -1,1 +1,35 @@
 #include "Timer.h"
+
+using namespace std::chrono;
+
+Timer::Timer() noexcept
+    : m_last(steady_clock::now())
+{ }
+
+float Timer::Peek() const noexcept
+{
+    duration<float> frameTime = steady_clock::now() - m_last;
+    return frameTime.count();
+}
+void Timer::Mark() noexcept
+{
+    float frameTime = Peek();
+    m_timeAmount += frameTime;
+    if (IsStop()) m_stopTimeAmount += frameTime;
+    m_last = steady_clock::now();
+}
+double Timer::Amount() const noexcept
+{
+    return m_timeAmount - m_stopTimeAmount;
+}
+
+void Timer::Stop() noexcept { m_isStop = true; }
+bool Timer::IsStop() const noexcept { return m_isStop; }
+void Timer::Resume() noexcept { m_isStop = false; }
+void Timer::Restart() noexcept
+{
+    m_isStop         = false;
+    m_timeAmount     = 0.0;
+    m_stopTimeAmount = 0.0;
+    m_last           = steady_clock::now();
+}
--- Practice/Timer.h
+++ Practice/Timer.h
@@ -1,5 +1,23 @@
 #pragma once
+#include <chrono>
+
 class Timer
 {
-};
+public:
+    Timer() noexcept;
+    float  Peek() const noexcept;
+    void   Mark() noexcept;
+    double Amount() const noexcept;
 
+    void Stop() noexcept;
+    bool IsStop() const noexcept;
+    void Resume() noexcept;
+    void Restart() noexcept;
+
+private:
+    std::chrono::time_point<std::chrono::steady_clock> m_last;
+
+    double m_timeAmount     = 0.0;
+    double m_stopTimeAmount = 0.0;
+    bool   m_isStop         = false;
+};
